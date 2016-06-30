45acaa9e3f1674a675c78c5c3737acfedf9c9046
Ference van Munster <ference@munstermedia.nl>
2016-06-30 18:44:33 +0200

    New files

---
+++ src/app.js
@@ -0,0 +1,199 @@
+'use strict';
+
+angular.module('app', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'client.controllers',
+  'waid.services',
+  'angular-growl',
+  'ui.bootstrap',
+  'angular-confirm',
+  'chart.js',
+  'textAngular'
+])
+.config(['growlProvider', function(growlProvider) {
+    growlProvider.globalTimeToLive(5000);
+}])
+  .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider, waidService) {
+    $routeProvider
+      .when('/social/error/:error/', {
+        template: '',
+        controller: 'ClientSocialError'
+      })
+      .when('/profile/emails/', {
+        templateUrl: 'waid/profile/emails.html',
+        resolve: {
+          authenticate: function(waidService){
+            return waidService.authenticate();
+          }
+        }
+      })
+      .when('/profile/main/', {
+        templateUrl: 'waid/profile/main.html',
+        resolve: {
+          authenticate: function(waidService){
+            return waidService.authenticate();
+          }
+        }
+      })
+      .when('/profile/username/', {
+        templateUrl: 'waid/profile/username.html',
+        resolve: {
+          authenticate: function(waidService){
+            return waidService.authenticate();
+          }
+        }
+      })
+      .when('/profile/password/', {
+        templateUrl: 'waid/profile/password.html',
+        resolve: {
+          authenticate: function(waidService){
+            return waidService.authenticate();
+          }
+        }
+      })
+      .when('/profile/interests/', {
+        templateUrl: 'waid/profile/interests.html',
+        resolve: {
+          authenticate: function(waidService){
+            return waidService.authenticate();
+          }
+        }
+      })
+      .when('/profile/overview/', {
+        templateUrl: 'waid/profile/overview.html',
+        resolve: {
+          authenticate: function(waidService){
+            return waidService.authenticate();
+          }
+        }
+      })
+      .when('/al/:code/', {
+        template: '',
+        controller: 'AutoLoginCtrl'
+      })
+      .when('/', {
+        templateUrl: 'waid/client/main.html',
+        resolve: {
+          authenticate: function(waidService){
+            // Authenticate, if logged in then you can redirect the user
+            waidService.authenticate();
+            // Just return true, otherwhise it will not load
+            return true
+          }
+        }
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+    $locationProvider.html5Mode(true);
+  }])
+  .run(['waidService',function(waidService){
+    waidService.initialize(
+      '/en/api',
+      config.accountId,
+      config.applicationId
+    );
+    waidService.authenticate();
+  }])
+  .controller('MasterCtrl', function ($scope, $rootScope, $location, $window, waidService, growl, $routeParams, $log) {
+    // Assume user is not logged in until we hear otherwise
+    $rootScope.authenticated = false;
+    $rootScope.showMenu = false;
+    $rootScope.initialized = false;
+
+    $rootScope.$on('waid.services.authenticate.ok', function(event, data) {
+      $rootScope.authenticated = true;
+      if ($location.path() == '/') {
+        $location.path("/profile/overview/");
+      }
+    });
+
+    $rootScope.$on('waid.services.authenticate.error', function(event, data) {
+      if ($location.path() != '/') {
+        $location.path("/");
+      }
+    });
+
+    $rootScope.$watch('authenticated', function(){
+      $scope.initVars();
+    });
+
+
+    $scope.initVars = function() {
+      if ($rootScope.authenticated) {
+        $rootScope.showMenu = true;
+      } else {
+        $rootScope.showMenu = false;
+      }
+    };
+
+    $scope.loginCheck = function(data) {
+      if (data.profile_status.length > 0) {
+        if(data.profile_status.indexOf('email_is_not_verified') !== -1) {
+          growl.addErrorMessage("Er is een activatie e-mail verstuurd. Controleer je e-mail om de login te verifieren.",  {ttl: -1});
+          $location.path('/');
+        }
+
+        if(data.profile_status.indexOf('profile_ok') !== -1) {
+          if ($location.path() != '/profile/overview/') {
+            $location.path('/profile/overview/')
+          }
+        }
+      }
+    };
+
+    $rootScope.$on('waid.services.application.userAutoLogin.get.ok', function(event, data) {
+      $scope.loginCheck(data);
+    });
+
+    $rootScope.$on('waid.services.application.userAutoLogin.get.error', function(event, data) {
+       $location.path("/");
+    });
+
+    $rootScope.$on('waid.services.application.userLogin.post.ok', function(event, data) {
+      $scope.loginCheck(data);
+    });
+
+    $rootScope.$on('waid.services.application.userLogout.post.ok', function(event, data) {
+      $rootScope.authenticated = false;
+      $location.path("/");
+    });
+    $rootScope.$on('waid.services.application.userLogoutAll.post.ok', function(event, data) {
+      $rootScope.authenticated = false;
+      $location.path("/");
+    });
+
+    $scope.$on('waid.services.application.userProfile.get.ok', function(event, data) {
+      $scope.userProfile = data;
+    });
+
+    $scope.$on('waid.services.application.userEmail.post.ok', function(event, data) {
+      growl.addSuccessMessage("Nieuw e-mail adres toegevoegd, controleer je mail om deze te verifieren.",  {ttl: -1});
+    });
+
+    $scope.$on('waid.services.application.userProfile.patch.ok', function(event, data) {
+      growl.addSuccessMessage("Profiel informatie opgeslagen");
+    });
+
+    $scope.$on('waid.services.application.userPassword.put.ok', function(event, data) {
+      growl.addSuccessMessage("Wachtwoord is gewijzigd.");
+    });
+
+     $scope.$on('waid.services.application.userLostLogin.post.ok', function(event, data) {
+      growl.addSuccessMessage("Instructies om in te loggen zijn naar jouw e-mail gestuurd.");
+    });
+    $scope.$on('waid.services.application.userRegister.post.ok', function(event, data) {
+      growl.addSuccessMessage("Geregistreerd als nieuwe gebruiker! Controleer je mail om de account te verifieren.",  {ttl: -1});
+      $scope.isRegister = true;
+    });
+    $scope.$on('waid.services.application.userCompleteProfile.post.ok', function(event, data) {
+      $scope.loginCheck(data);
+    });
+
+    $scope.getMenuActiveClass = function (path) {
+      return ($location.path().substr(0, path.length) === path) ? 'active' : '';
+    }
+  });
---
+++ src/controllers.js
@@ -0,0 +1,345 @@
+'use strict';
+
+angular.module('client.controllers', ['app'])
+  .controller('ClientSocialError', function ($scope, $rootScope, growl, $routeParams, $location) {
+    growl.addErrorMessage(config.errorCodes[$routeParams.error]);
+    $location.path("/");
+  }) 
+  .controller('DefaultModalCtrl', function ($scope, $location, waidService,  $uibModalInstance) {
+    $scope.close = function () {
+      $uibModalInstance.dismiss('close');
+    };
+  }) 
+
+  .controller('CompleteProfileCtrl', function ($scope, $location, $window, waidService,  $uibModalInstance) {
+    $scope.mode = 'complete';
+    $scope.close = function () {
+      $uibModalInstance.dismiss('close');
+    };
+  }) 
+
+  .controller('MainCtrl', function ($scope, $location, waidService,  $uibModal) {
+    $scope.completeProfile = function () {
+      var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'waid/client/complete-profile.html',
+        controller: 'CompleteProfileCtrl',
+        size: 'lg',
+        resolve: {
+          account: function () {
+            return $scope.account;
+          }
+        }
+      });
+    }
+
+    $scope.lostLogin = function () {
+      var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'waid/client/lost-login-modal.html',
+        controller: 'DefaultModalCtrl',
+        size: 'lg',
+        resolve: {
+          account: function () {
+            return $scope.account;
+          }
+        }
+      });
+    }
+  }) 
+  .controller('LoginCtrl', function ($scope, $location, waidService) {
+
+    $scope.model = {
+      'username':'',
+      'password':''
+    };
+
+    $scope.errors = [];
+
+    $scope.login = function() {
+      waidService.userLoginPost($scope.model)
+        .then(function(data){
+          if(typeof data.profile_status != "undefined" && data.profile_status.indexOf('missing_profile_data') !== -1) {
+            $scope.completeProfile();
+          }
+        },function(data){
+          $scope.errors = data;
+        }
+      );
+    }
+  })
+  .controller('LostLoginCtrl', function ($scope, $location, waidService) {
+
+    $scope.model = {
+      'email':'',
+    };
+
+    $scope.errors = [];
+
+    $scope.submit = function() {
+      waidService.userLostLoginPost($scope.model)
+        .then(function(data){
+          $scope.errors = [];
+          $scope.close();
+        },function(data){
+          $scope.errors = data;
+        }
+      );
+    }
+  }) 
+  .controller('AutoLoginCtrl', function ($scope, $location, waidService, $routeParams) {
+    waidService.userAutoLoginGet($routeParams.code).then(function(data) {
+      if(typeof data.profile_status != "undefined" && data.profile_status.indexOf('missing_profile_data') !== -1) {
+        $scope.completeProfile(data);
+      }
+    });
+  })  
+  // .controller('ProfileLoginCtrl', function ($scope, $rootScope, $location, waidService) {
+  //   $scope.currentPanel = 'login';
+  // })
+  .controller('ProfileInterestsCtrl', function ($scope, $rootScope, $location, waidService) {
+    // $scope.currentPanel = 'interests';
+    $scope.model = {};
+
+    waidService.userProfileGet().then(function(data) {
+      $scope.errors = [];
+      $scope.model = data;
+    }, function(data) {
+      $scope.errors = data;
+    });
+
+    $scope.save = function(){
+      waidService.userProfilePatch($scope.model).then(function(data) {
+        $scope.errors = [];
+        $location.path("/profile/overview/");
+      }, function(data) {
+        $scope.errors = data;
+      });
+    }
+  })
+
+  .controller('ProfileOverviewCtrl', function ($scope, $rootScope, $location, waidService) {
+    waidService.userProfileGet().then(function(data) {
+      $scope.errors = [];
+      $scope.model = data;
+    }, function(data) {
+      $scope.errors = data;
+    });
+
+    waidService.userEmailListGet().then(function(data) {
+        $scope.emails = data;
+    });
+  })
+
+  .controller('ProfileMainCtrl', function ($scope, $rootScope, $location, waidService, $filter) {
+    $scope.model = {};
+
+    $scope.dateOptions = {
+      dateDisabled: false,
+      maxDate: new Date(),
+      minDate: new Date(1940, 1, 1),
+      startingDay: 1,
+      datepickerMode: 'year'
+    };
+
+    
+    $scope.popup = {
+      opened: false
+    };
+    $scope.open = function() {
+      $scope.popup.opened = true;
+    };
+
+    waidService.userProfileGet().then(function(data) {
+      $scope.errors = [];
+      if (data.date_of_birth) {
+        var dateParts = data.date_of_birth.split('-')
+        data.date_of_birth = new Date(dateParts[0],dateParts[1]-1,dateParts[2]);
+      }
+      $scope.model = data;
+    }, function(data) {
+      $scope.errors = data;
+    });
+
+    $scope.save = function(){
+      $scope.model.date_of_birth = $filter('date')($scope.model.date_of_birth, 'yyyy-MM-dd');
+
+      waidService.userProfilePatch($scope.model).then(function(data) {
+        $scope.errors = [];
+        $location.path("/profile/overview/");
+      }, function(data) {
+        $scope.errors = data;
+      });
+    }
+  })
+  .controller('ProfilePasswordCtrl', function ($scope, $rootScope, $location, waidService, $filter) {
+    $scope.model = {};
+
+    $scope.save = function(){
+      waidService.userPasswordPut($scope.model).then(function(data) {
+        $scope.errors = [];
+        $location.path("/profile/overview/");
+      }, function(data) {
+        $scope.errors = data;
+      });
+    }
+  })
+  .controller('ProfileUsernameCtrl', function ($scope, $rootScope, $location, waidService, $filter) {
+    //$scope.model = {};
+
+    waidService.userProfileGet().then(function(data) {
+      $scope.errors = [];
+      $scope.model = {'username':data.username};
+    }, function(data) {
+      $scope.errors = data;
+    });
+
+    $scope.save = function(){
+      waidService.userUsernamePut($scope.model).then(function(data) {
+        $scope.errors = [];
+        $location.path("/profile/overview/");
+      }, function(data) {
+        $scope.errors = data;
+      });
+    }
+  })
+  .controller('ProfileEmailCtrl', function ($scope, $rootScope, $location, waidService) {
+    $scope.inactiveEmails = [];
+    $scope.activeEmails = [];
+
+    $scope.emailAdd = '';
+
+    $scope.initEmails = function(data) {
+      $scope.inactiveEmails = [];
+      $scope.activeEmails = [];
+
+      if (data.length > 0) {
+        for (var i=0; i<data.length; i++) {
+          if (data[i].is_verified == 1) {
+            $scope.activeEmails.push(data[i]);
+          } else {
+            $scope.inactiveEmails.push(data[i]);
+          }
+        }
+      }
+    };
+
+    $scope.deleteEmail = function(id) {
+      waidService.userEmailDelete(id).then(function(data) {
+        $scope.errors = [];
+        $scope.loadEmailList();
+      }, function(data) {
+        $scope.errors = data;
+      });
+    };
+
+    $scope.addEmail = function() {
+      var data = {'email': $scope.emailAdd}
+      waidService.userEmailPost(data).then(function(data) {
+        $scope.errors = [];
+        $scope.loadEmailList();
+        $scope.emailAdd = '';
+      }, function(data) {
+        $scope.errors = data;
+      });
+    };
+
+    $scope.loadEmailList = function() {
+      waidService.userEmailListGet().then(function(data) {
+        $scope.initEmails(data);
+      });
+    };
+
+    $scope.loadEmailList();
+    
+  })
+  .controller('SocialCtrl', function ($scope, $location, waidService) {
+    $scope.providers = [];
+    $scope.getProviders = function() {
+      waidService.socialProviderListGet().then(function(data){
+        $scope.providers = data;
+      });
+    }
+    $scope.getProviders();
+  })
+  .controller('LogoutCtrl', function ($scope, $location, waidService) {
+    $scope.logout = function() {
+      waidService.userLogoutPost();
+    }
+    $scope.logoutAll = function() {
+      waidService.userLogoutAllPost();
+    }
+  })
+  .controller('RegisterCtrl', function ($scope, $route, waidService, $location, $uibModal) {
+    $scope.show = {};
+    $scope.missingEmailVerification = false;
+    if ($scope.modus == 'complete') {
+      // Check for logged-in user
+      waidService.userCompleteProfileGet().then(function(data) {
+        $scope.model = data.user;
+        if(typeof data.profile_status != "undefined" && data.profile_status.indexOf('email_is_not_verified') !== -1) {
+           $scope.missingEmailVerification = true;
+        }
+
+        // Set missing data
+        for (var i=0; i < data.missing_data.length; i++) {
+          $scope.show[data.missing_data[i]] = true;
+        }
+      }, function(data) {
+        // Not logged in
+      });
+    } else {
+      $scope.missingEmailVerification = false;
+      $scope.show = {
+        'username':true,
+        'password':true,
+        'email':true,
+        'terms_and_conditions_check':true
+      }
+    }
+
+
+    $scope.openTermsAndConditions = function (template) {
+      var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'waid/client/terms-and-conditions.html',
+        controller: 'DefaultModalCtrl',
+        size: 'lg',
+        resolve: {
+          application: function () {
+            return $scope.application;
+          }
+        }
+      });
+    };
+
+    // $scope.model = {
+    //   'username':'',
+    //   'password':'',
+    //   'email':'',
+    //   'terms_and_conditions_check':false
+    // };
+    $scope.register = function(){
+      if ($scope.model.terms_and_conditions_check) {
+        $scope.errors = [];
+        if ($scope.modus == 'complete') {
+          waidService.userCompleteProfilePost($scope.model)
+            .then(function(data){
+              $scope.model = {};
+              $scope.close();
+            },function(data){
+              $scope.errors = data;
+            }
+          );
+        } else {
+          waidService.userRegisterPost($scope.model)
+            .then(function(data){
+              $scope.model = {};
+            },function(data){
+              $scope.errors = data;
+            }
+          );
+        }
+      }
+    }
+  });
